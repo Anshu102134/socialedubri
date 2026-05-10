2ccac1bb494e308f3657ee773c6da325c8694c60
Anshu102134 <anshusingh29032004@gmail.com>
2026-05-10 22:55:43 +0530

    Final connection and video fix

--- frontend2/src/layouts/MainLayout.js
+++ frontend2/src/layouts/MainLayout.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import backgroundVideo from '../background-video.mp4';
-
 const MainLayout = () => {
     return (
         <div className="main-layout">
             <video autoPlay loop muted className="bg-video">
-                <source src={backgroundVideo} type="video/mp4" />
+                <source src="/background-video.mp4" type="video/mp4" />
             </video>
             <header>
                 <Navbar />
